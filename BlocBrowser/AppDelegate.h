109d2eab5fcf7f5dd2fe182dc44240c22ac19ade
Julicia James <d.james128@gmail.com>
2014-11-19 22:13:50 -0500

    Initial Commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Julicia on 11/19/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Julicia on 11/19/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
