012ae98d54d7cd8f30700c2d83e0dbe60f9895da
Julicia James <d.james128@gmail.com>
2014-11-30 16:50:16 -0500

    Automatically clear history when the app is closed

--- BlocBrowser/BLCWebBrowserViewController.h
+++ BlocBrowser/BLCWebBrowserViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface BLCWebBrowserViewController : UIViewController
+/**
+ Replaces the web view with a fresh one, erasing all history. Also updates the URL field and toolbar buttons appropriately.
+ */
+- (void) resetWebView;
 
 @end
--- BlocBrowser/BLCWebBrowserViewController.m
+++ BlocBrowser/BLCWebBrowserViewController.m
@@ -29,6 +29,12 @@
 #pragma mark - UIViewController
 
 - (void)loadView {
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Welcome", @"Welcome title")
+                                                    message:NSLocalizedString(@"Get ready to explore the rugged terrain of the interwebs incognito.", @"Welcome comment")
+                                                   delegate:nil
+                                          cancelButtonTitle:NSLocalizedString(@"OK, I'm ready!", @"Welcome button title") otherButtonTitles:nil];
+    [alert show];
+
     UIView *mainView = [UIView new];
     
     self.webView = [[UIWebView alloc] init];
@@ -56,18 +62,12 @@
     [self.reloadButton setEnabled:NO];
     
     [self.backButton setTitle:NSLocalizedString(@"Back", @"Back comnmand") forState:UIControlStateNormal];
-    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.forwardButton setTitle:NSLocalizedString(@"Forward", @"Forward comnmand") forState:UIControlStateNormal];
-    [self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.stopButton setTitle:NSLocalizedString(@"Stop", @"Stop comnmand") forState:UIControlStateNormal];
-    [self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.reloadButton setTitle:NSLocalizedString(@"Refresh", @"Reload comnmand") forState:UIControlStateNormal];
-    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
     
-    
+    [self addButtonTargets];
+
     
     
     for (UIView *viewToAdd in @[self.webView, self.textField, self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
@@ -171,6 +171,33 @@
 
 }
 
+- (void) addButtonTargets {
+    for (UIButton *button in @[self.backButton, self.forwardButton, self.stopButton, self.reloadButton]) {
+        [button removeTarget:nil action:NULL forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    [self.backButton addTarget:self.webView action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
+    [self.forwardButton addTarget:self.webView action:@selector(goForward) forControlEvents:UIControlEventTouchUpInside];
+    [self.stopButton addTarget:self.webView action:@selector(stopLoading) forControlEvents:UIControlEventTouchUpInside];
+    [self.reloadButton addTarget:self.webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void) resetWebView {
+    [self.webView removeFromSuperview];
+    
+    UIWebView *newWebView = [[UIWebView alloc] init];
+    newWebView.delegate = self;
+    [self.view addSubview:newWebView];
+    
+    self.webView = newWebView;
+    
+    [self addButtonTargets];
+    
+    self.textField.text = nil;
+    [self updateButtonsAndTitle];
+}
+
+
 #pragma mark - Miscellaneous
 
 - (void) updateButtonsAndTitle {
@@ -192,7 +219,7 @@
     self.backButton.enabled = [self.webView canGoBack];
     self.forwardButton.enabled = [self.webView canGoForward];
     self.stopButton.enabled = self.frameCount > 0;
-    self.reloadButton.enabled = self.frameCount == 0;
+    self.reloadButton.enabled = self.webView.request.URL && self.frameCount == 0;
 
 }
 
